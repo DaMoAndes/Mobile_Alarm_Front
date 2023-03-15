28330ac41f8c648475223d52a83792ef55eb4170
DaMoAndes <d.moralesa2@uniandes.edu.co>
2023-03-15 18:04:24 -0500

    Mobile_Alarma_Front

---
+++ app/src/main/java/com/example/mobile_alarma_front/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobile_alarma_front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.alarma_listar)
+    }
+}
